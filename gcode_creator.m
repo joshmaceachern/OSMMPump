79b4fbc97c8cd7b8a4dadb5bd739b5d08d24d1fd
joshmaceachern <64046084+joshmaceachern@users.noreply.github.com>
2020-06-15 20:26:52 -0800

    Fixed flow rate input
    Added input/warnings for flow rate addition

--- gcode_creator.m
+++ gcode_creator.m
@@ -3,10 +3,10 @@
 
 if exist("loc")
   if isempty(loc)
-   loc = inputdlg("What would you like to name your file?")
+   loc = inputdlg("What would you like to name your file?");
    end
 else
-    loc = inputdlg("What would you like to name your file?")
+    loc = inputdlg("What would you like to name your file?");
 end
 
 %CHECK TO SEE IF THE FILE ALREADY EXISTS
@@ -15,22 +15,22 @@ if exist(strcat(char(loc), ".gcode"))
   prompt = strcat("The file", " ", char(loc), ".gcode already exists.");
   replace = questdlg(prompt,'Continue?', "Replace", 'Append', 'Replace');
   if strcmp(replace, "Replace")
-    gCodeFile = fopen(strcat(char(loc), ".gcode"), 'w')
+    gCodeFile = fopen(strcat(char(loc), ".gcode"), 'w');
     %gCodeFile = []
   elseif strcmp(replace, "Append")
-    gCodeFile = fopen(strcat(char(loc), ".gcode"), 'a')
+    gCodeFile = fopen(strcat(char(loc), ".gcode"), 'a');
   else
     return
   end
 else
-  gCodeFile = fopen(strcat(char(loc), ".gcode"), 'a')
+  gCodeFile = fopen(strcat(char(loc), ".gcode"), 'a');
 end
 
-%GET INPUT AMOUNTS FROM USER
+%GET INPUT VOLUME FROM USER
 prompt = {"Motor A", "Motor B", "Motor C", "Motor D", "Motor E"};
 defaults = {"0.0", "0.0", "0.0", "0.0", "0.0"};
 rowscols = [1,25;1,25;1,25;1,25;1,25];
-title = "Enter pump volume in mL"
+title = "Enter pump volume in mL";
 amounts = inputdlg(prompt,title, rowscols, defaults);
 volumeA = char(amounts(1));
 volumeB = char(amounts(2));
@@ -38,14 +38,23 @@ volumeC = char(amounts(3));
 volumeD = char(amounts(4));
 volumeE = char(amounts(5));
 
+
+                
+%%GET INPUT FLOW RATE FROM USER
+prompt = {'Enter Volumetric Flow Rate in uL/min'};
+defaults = {[]};
+rowscols = [1, 20];
+title = "Flow Rate";
+flowRate = inputdlg(prompt, title, rowscols, defaults);;
+flowRate = char(flowRate);
+
+%CREATE GCODE LINE
 gcode = cstrcat("X", volumeA, ' ', ...
                 "Y", volumeB, " ", ...
                 "Z", volumeC, " ", ...
                 "I", volumeD, " ", ...
-                "J", volumeE)
-                
-                
-                
+                "J", volumeE, " ", ...
+                "F", flowRate)
 %%OUTPUT IF NO VOLUME SPECIFIED                
 if str2num(volumeA) == 0
   fprintf("No output for pump A declared, assuming 0 \n")
@@ -70,13 +79,19 @@ end
 
 if str2num(volumeD) == 0
   fprintf("No output for pump D declared, assuming 0 \n")
-    loc1 = strfind(gcode, "I");
+  loc1 = strfind(gcode, "I");
   loc2 = strfind(gcode, "J");
   gcode(loc1:loc2-1) = [];
   end
 if str2num(volumeE) == 0
   fprintf("No output for pump E declared, assuming 0 \n")
   loc1 = strfind(gcode, "J");
+  loc2 = strfind(gcode, "F");
+  gcode(loc1:loc2-1) = [];
+end
+if isempty(flowRate)
+  fprintf("No flow rate declared. Keeping previous/default\n")
+  loc1 = strfind(gcode, "F");
   gcode(loc1:end) = [];
 end
 %WARNING IF GCODE LINE IS EMPTY            
@@ -100,7 +115,7 @@ end
 if strcmp(cont, "Yes")
   gcode_creator
 end
- if !strcmp(cont, "Yes")
+ if !strcmp(cont, "Yes");
   loc = [];
 end
-fclose(gCodeFile)
+fclose(gCodeFile);
