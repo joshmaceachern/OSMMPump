68577b1670a1cafa329a125432dff8e41c888e06
joshmaceachern <64046084+joshmaceachern@users.noreply.github.com>
2020-06-25 15:57:38 -0800

    General Update/New code
    General design update, with minor fix issues. Added in rollers to reduce friction from the pins on the hose. This is represented by a new parameter in the F3d file. The rollers currently being used are nylon spacers for a #4 screw, as they are readily available. 
    The Steps per uL calculator was also created. This takes the hose dimensions, and pump dimensions such as the hose bend radius and outputs what the steps/mm should be. 
    Next steps are getting the Marlin fork to work to support five "axes"

---
+++ Steps_per_uL_calculator.m
@@ -0,0 +1,25 @@
+%GET INPUTS FROM USER
+prompts = {'Enter Hose ID (mm)', 'Enter Hose Bend Radius (mm)',...
+ 'Enter Steps per revolution of motor', 'Enter number of microsteps (1, 2, 4, 8, 16)', 'Enter Gear Ratio'};
+dlgtitle = 'Input';
+dims = [1,25;1,25; 1,25;1,25;1,25];
+inputs = inputdlg(prompts, dlgtitle, dims);
+hoseID = str2num(inputs{1});
+
+%CALCULATE OVERALL VOLUME
+%A = pi * d^2 * 0.25
+crossSectionalArea = (pi*(hoseID^2))*.25; %%mm^2
+%C = pi*d. Input is radius, and we only need 1/2 of a circle, so it simplifies to pi*r
+lengthOAL = pi*str2num(inputs{2});
+%V = A*L
+volume = crossSectionalArea*lengthOAL;
+
+%CALCULATE THE VOLUME PER STEP
+stepsRev = str2num(inputs{2})*str2num(inputs{3})*str2num(inputs{4});
+volPerStep = volume/stepsRev;
+stepsPerVol = stepsRev/volume;
+
+%DISPLAY CALCULATED OUTPUT
+message = strcat("Volume per Step:", " ", num2str(volPerStep), "\n",...
+                  "Steps per uL:", " ", num2str(stepsPerVol));
+msgbox(message);
